0a0c0b424cccf2385559190442be9545ec27cbe3
Lara <lara245@gmail.com>
2021-12-16 22:55:17 -0300

    aula_16_12

--- AppList.js
+++ AppList.js
@@ -1,12 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import AppItem from "./AppItem";
 
 export default function AppList() {
+  const [items, setItems] = useState([
+    { id: 1, quantidade: 5, descricao: "arroz" },
+    { id: 2, quantidade: 1, descricao: "feijão" },
+    { id: 3, quantidade: 0.5, descricao: "lentilha" },
+    { id: 4, quantidade: 1, descricao: "massa" },
+    { id: 5, quantidade: 1, descricao: "katchup" },
+    { id: 6, quantidade: 1, descricao: "queijo-ralado" },
+    { id: 7, quantidade: 5, descricao: "arroz" },
+    { id: 8, quantidade: 1, descricao: "feijão" },
+    { id: 9, quantidade: 0.5, descricao: "lentilha" },
+    { id: 10, quantidade: 1, descricao: "massa" },
+    { id: 11, quantidade: 1, descricao: "katchup" },
+    { id: 6, quantidade: 1, descricao: "queijo-ralado" },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>List!</Text>
+    <View style={{ flex: 1 }}>
       <StatusBar style="light" />
+      <Text style={styles.title}>Lista de Compras</Text>
+      <ScrollView>
+        {items.map((item) => {
+          return (
+            <AppItem
+              key={item.id}
+              id={item.id}
+              item={item.quantidade + "  de " + item.descricao}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -17,5 +44,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#D93600",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  scrollContainer: {
+    flex: 1,
+    width: "90%",
+  },
+  itemsContainer: {
+    flex: 1,
+    marginTop: 10,
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: "stretch",
+    backgroundColor: "#fff",
   },
 });
